29460a847d0cb9502b58f031e1027ef55dd2fce7
Henrik Knudsen <henrik@csolutions.no>
2019-05-13 21:14:11 +0200

    Data persisted using User Defaults Method

--- Bouwer ToDo/TodoListViewController.swift
+++ Bouwer ToDo/TodoListViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     var itemArray = ["Find Mike", "Feed Cat", "Feed Dog"]
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
 
     }
     //Creates number of rows
@@ -53,7 +56,11 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //What will happen once the user clicks the add item button on our  UI ALrt
-           self.itemArray.append(textField.text!)
+            
+            self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            
             self.tableView.reloadData()
             
         }
